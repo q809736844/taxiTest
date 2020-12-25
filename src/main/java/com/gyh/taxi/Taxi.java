9c44f794bb04133401478627e05834f5d8a242ea
guoyunhao <809736844@qq.com>
2020-12-25 11:19:06 +0800

    新增出租车实体类

--- src/main/java/com/gyh/taxi/Taxi.java
+++ src/main/java/com/gyh/taxi/Taxi.java
@@ -1,4 +1,62 @@
 package com.gyh.taxi;
 
+import com.gyh.utils.DateUtil;
+
+import java.util.Date;
+
 public class Taxi {
+    private String carNo;
+    private Date buyDate;
+    private String BrandName;
+    private Long runningKito;
+    private boolean isRepair;
+
+    public Taxi(String catLine) {
+        String[] carMessages = catLine.split("|");
+        this.carNo = carMessages[0];
+        this.buyDate = DateUtil.parseDate(carMessages[1]);
+        BrandName = carMessages[2];
+        this.runningKito = Long.parseLong(carMessages[0]);
+        this.isRepair = "T".equals(carMessages[4])?true:false;
+    }
+
+    public String getCarNo() {
+        return carNo;
+    }
+
+    public void setCarNo(String carNo) {
+        this.carNo = carNo;
+    }
+
+    public Date getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(Date buyDate) {
+        this.buyDate = buyDate;
+    }
+
+    public String getBrandName() {
+        return BrandName;
+    }
+
+    public void setBrandName(String brandName) {
+        BrandName = brandName;
+    }
+
+    public Long getRunningKito() {
+        return runningKito;
+    }
+
+    public void setRunningKito(Long runningKito) {
+        this.runningKito = runningKito;
+    }
+
+    public boolean isRepair() {
+        return isRepair;
+    }
+
+    public void setRepair(boolean repair) {
+        isRepair = repair;
+    }
 }
